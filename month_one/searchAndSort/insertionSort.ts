171fd5440eae4a6103d35bb161c24df9c81354b1
aditya kumar <aaditya1392@gmail.com>
2025-08-06 23:19:57 +0530

    starting insertion sort

---
+++ month_one/searchAndSort/insertionSort.ts
@@ -0,0 +1,6 @@
+function insertionSort(arr: number[]) {}
+
+const arrTosortUsingInsertion = [1, 2, 1, 5, 3, 20, 12, 11, 10];
+console.log(arrTosortUsingInsertion);
+insertionSort(arrTosortUsingInsertion);
+console.log(arrTosortUsingInsertion);
